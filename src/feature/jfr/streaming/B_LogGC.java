4c97564ad21d6247506841198125210efce47491
Jack Shirazi <jack.shirazi@elastic.co>
2022-05-24 18:22:47 +0100

    note command line -Xlog:gc*=info in B_LogGC example

--- src/feature/jfr/streaming/B_LogGC.java
+++ src/feature/jfr/streaming/B_LogGC.java
@@ -3,6 +3,7 @@ package feature.jfr.streaming;
 public class B_LogGC {
 
     public static void main(String[] args) {
+        //Run with command line option: -Xlog:gc*=info
         A_TestApp.main(null);
     }
 
